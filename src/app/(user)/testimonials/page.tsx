10f367189c8f2354f22bc29bf92ccf72c4d9531e
sammykirigha <dkirigha18@gmail.com>
2025-04-04 15:27:43 +0300

    updated the page

--- src/app/(user)/testimonials/page.tsx
+++ src/app/(user)/testimonials/page.tsx
@@ -9,7 +9,7 @@ const getData = async () => {
 const Page = async () => {
   const data = await getData()
   return (
-    <div className='max-w-[1240px] mx-auto w-full mt-[100px] sm:py-6 lg:py-10'>
+    <div className='max-w-[1240px] mx-auto w-full mt-[150px] sm:py-6 lg:py-10'>
       <h2 className='w-full text-center text-[25px] font-[700] my-5 text-[#239492] leading-3'>The Impact of Our Program</h2>
       <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-5 mx-5 mt-6'>
         {data.map((testimonial, index) => {
